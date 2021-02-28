2565ea40c376191e10ec9f5fb7c03f0f9650d509
ForkBug <ForkBug@github.com>
2021-02-28 22:37:50 +0800

    Update nuget

--- Source/DokanFS.Core/DokanFileSystemAdapter.cs
+++ Source/DokanFS.Core/DokanFileSystemAdapter.cs
@@ -610,7 +610,6 @@ namespace DokanFS
             }
             catch
             {
-                volumeLabel = "Personal Cloud";
                 volumeLabel = "Personal House";
                 fileSystemName = "NTFS";
                 maximumComponentLength = 256;
